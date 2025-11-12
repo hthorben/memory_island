e743892e24d399997608c1880825c6c1df8a0d97
Thorben <hthorben@ethz.ch>
2025-11-12 11:19:16 +0100

    added impl signal distribution +k

--- src/memory_island_core.sv
+++ src/memory_island_core.sv
@@ -722,7 +722,7 @@ module memory_island_core #(
 
       for (genvar k = 0; k < NumPhysicalBanks; k++) begin : assign_pwr_control_sigs
         //assign impl_narrow[k] = impl_i[ (j/GatingGranularity) + ((k*NWDivisor*NumWideBanks)/GatingGranularity) + ((i*NWDivisor) / GatingGranularity)];
-        assign impl_narrow[k] = impl_i[(NUM_PWR_DOMAINS * (k + i * NumPhysicalBanks * NWDivisor))/(NumWideBanks * NumPhysicalBanks * NWDivisor)];
+        assign impl_narrow[k] = impl_i[(NUM_PWR_DOMAINS * (k + i * NumPhysicalBanks * NWDivisor))/(NumWideBanks * NumPhysicalBanks * NWDivisor) + k];
       end
 
       // Memory bank
